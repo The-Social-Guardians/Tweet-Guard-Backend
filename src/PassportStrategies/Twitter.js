5f66c35a4a9a726f953e346645c7d279567ed85f
Dunsin <jesudunsinfaiye@gmail.com>
2022-08-10 10:58:40 +0000

    feat(twitter.js): save user details
    fix #4

--- src/PassportStrategies/Twitter.js
+++ src/PassportStrategies/Twitter.js
@@ -1,4 +1,6 @@
 import { Strategy } from 'passport-twitter';
+import UserModel from '../Models/UserModel';
+import UserAccessTokenModel from '../Models/UserAccessTokenModel';
 
 //check here for reference => https://www.passportjs.org/packages/passport-twitter/
 export default function (passport) {
@@ -12,7 +14,12 @@ export default function (passport) {
                     'https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true',
             },
             async (token, tokenSecret, profile, cb) => {
-                cb(null, profile);
+                UserModel.findById({twitterId: profile.id}, (error, user) => {
+                    if(error) {
+                        UserModel.create({twitterId: profile.id})
+                    }
+                    cb(null, user)
+                })
             }
         )
     );
