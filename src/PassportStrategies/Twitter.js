8b220c7142405657bb6fa3ed1474896f281d8322
Dunsin <jesudunsinfaiye@gmail.com>
2022-08-11 10:09:29 +0000

    feat(twitter.js): save user to details
    closes #4

--- src/PassportStrategies/Twitter.js
+++ src/PassportStrategies/Twitter.js
@@ -2,6 +2,16 @@ import { Strategy } from 'passport-twitter';
 import UserModel from '../Models/UserModel';
 import UserAccessTokenModel from '../Models/UserAccessTokenModel';
 
+function getCurrentDate(){
+    let today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy;
+    return today;
+}
+
 //check here for reference => https://www.passportjs.org/packages/passport-twitter/
 export default function (passport) {
     passport.use(
@@ -14,12 +24,29 @@ export default function (passport) {
                     'https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true',
             },
             async (token, tokenSecret, profile, cb) => {
-                UserModel.findById({twitterId: profile.id}, (error, user) => {
-                    if(error) {
-                        UserModel.create({twitterId: profile.id})
-                    }
-                    cb(null, user)
-                })
+               try{
+                const userExists = await UserModel.findOne({twitterId: profile.id})
+
+                if(userExists) {
+                    return cb(null, userExists.lean())
+                } else {
+                    const newUser = UserModel.create({
+                        twitterInfo: {
+                            id: profile.id
+                        },
+                        registeredAt: getCurrentDate()
+                    })
+
+                    UserAccessTokenModel.create({
+                        user: newUser,
+                        accessToken: token,
+                        accessTokenSecret: tokenSecret
+                    })
+                    return cb(null, newUser.json())
+                }
+               } catch(e) {
+                return cb(error)
+               }
             }
         )
     );
