fd2a50b3334dcd55d6052962a288b91631dda996
Mathias Ayivor <mathiasayivor5@gmail.com>
2022-08-21 23:55:12 +0000

    fix(mongoose): changed imports to be compatible with mongoose's commonJS Module

--- src/Models/ShameListModel.js
+++ src/Models/ShameListModel.js
@@ -1,4 +1,5 @@
-import { model, Schema} from 'mongoose';
+import mongoose from "mongoose";
+const { model, Schema } = mongoose;
 
 const ShameListSchema = new Schema({
     _id: Schema.Types.ObjectId,
