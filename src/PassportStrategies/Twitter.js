89cff57ac01dbbc9734caa15f1df946dcc64b6ad
Mathias Ayivor <mathiasayivor5@gmail.com>
2022-08-30 10:28:36 +0000

    feat(twitter passport strategy): added typing support for passport param

--- src/PassportStrategies/Twitter.js
+++ src/PassportStrategies/Twitter.js
@@ -1,4 +1,5 @@
 import { Strategy } from 'passport-twitter';
+import { PassportStatic } from 'passport';
 import UserModel from '../Models/UserModel.js';
 import UserAccessTokenModel from '../Models/UserAccessTokenModel.js';
 import { isDevMode } from '../Lib/utils.js';
@@ -14,6 +15,10 @@ function getCurrentDate() {
 }
 
 //check here for reference => https://www.passportjs.org/packages/passport-twitter/
+/**
+ * 
+ * @param {PassportStatic} passport 
+ */
 export default function (passport) {
     passport.use(
         new Strategy(
