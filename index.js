693d140495faea97fb7a75f24fcc9cb5113efed0
Mathias Ayivor <mathiasayivor5@gmail.com>
2022-08-31 01:28:13 +0000

    feat(exceptions): added exception handling middleware

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ import { connect as connectDb } from './src/Lib/dbConnection.js';
 import indexRouter from './src/Routes/index.js'
 import authRouter from './src/Routes/auth.js'
 import apiRouter from './src/Routes/api.js'
+import HttpException from './src/Exceptions/HttpException.js'
+import InternalServerException from './src/Exceptions/InternalServerException.js';
+import { isDevMode } from './src/Lib/utils.js';
 
 config();
 connectDb();
@@ -15,6 +18,22 @@ const app = express();
 app.use('/', indexRouter())
 app.use('/auth', authRouter())
 app.use('/api', apiRouter())
+app.use((err, req, res, next) => {
+    if (!(err instanceof HttpException)) {
+        throw new InternalServerException(err)
+    }
+
+    const response = typeof err.getMessage() == 'object' && err.getMessage() !== null ? err.getMessage() : {
+        message: err.getMessage(),
+    }
+
+    if (isDevMode()) {
+        response.stack = err.stack
+        response.name = err.name
+    }
+
+    res.status(err.getCode()).json(response)
+})
 
 app.listen(PORT, () => {
     console.log('Server listening on ', PORT);
