cfa381058c51fdc36a97baa0c804b1eed8324347
Mathias Ayivor <mathiasayivor5@gmail.com>
2022-08-31 01:34:33 +0000

    feat(auth): fully implemented jwt authentication for /api/* endpoints
    addresses #5

--- src/Controllers/AuthController.js
+++ src/Controllers/AuthController.js
@@ -1,14 +1,35 @@
 import passport from "passport";
+import jwt from 'jsonwebtoken';
 import { isDevMode } from "../Lib/utils.js";
 
 const twitterAuth = passport.authenticate('twitter');
 
 //this callback is called automatically after auth : reference => https://www.passportjs.org/packages/passport-twitter/
-const twitterAuthCallback = passport.authenticate('twitter', {
-    assignProperty: 'federatedUser',
-    failureRedirect: 'http://localhost:5000/failure', // auth failure redirect,
-    successRedirect: isDevMode() ? 'http://localhost:5000/success' : 'https://tweet-guard-frontend.vercel.app', //auth success redirect
-})
+/** @type {import("express").RequestHandler} */
+const twitterAuthCallback = (req, res, next) => {
+    passport.authenticate('twitter', {
+        assignProperty: 'federatedUser',
+        failureRedirect: 'http://localhost:5000/failure', // auth failure redirect,
+    }, async (err, user, info, status) => {
+        if (!user) {
+            res.json({
+                message: 'An unknown error occured'
+            }).status(500)
+            return;
+        }
+
+        const token = jwt.sign({
+            user: {
+                id: user._id,
+                name: user.twitterUsername
+            }
+        }, process.env.JWT_SECRET, {
+            expiresIn: '1h'
+        });
+
+        res.redirect(isDevMode() ? `http://localhost:5000/success?token=${token}` : `https://tweet-guard-frontend.vercel.app?token=${token}`) //auth success redirect
+    })(req, res, next)
+}
 
 export {
     twitterAuth,
