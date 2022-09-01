c9aeddb60a99340ec420200119ccab4f1af01041
Mathias Ayivor <mathiasayivor5@gmail.com>
2022-09-01 11:43:43 +0000

    feat(utils): added createController function to help with typing support for controller methods

--- src/Lib/utils.js
+++ src/Lib/utils.js
@@ -1,7 +1,16 @@
 export function isDevMode() {
     if (process.env.NODE_ENV) {
-        return /^dev(elopment)?$/i.test(process.env.NODE_ENV)
+        return /^dev(elopment)?$/i.test(process.env.NODE_ENV);
     }
 
     return true;
+}
+
+/**
+ * @param {import("express").RequestHandler} controllerMethod
+ *
+ * @returns {import("express").RequestHandler}
+ */
+export function createController(controllerMethod) {
+    return controllerMethod;
 }
