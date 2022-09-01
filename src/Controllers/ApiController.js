9fb74b2d5ec09aecb0a0aed983b2e325cb7f4bb2
Mathias Ayivor <mathiasayivor5@gmail.com>
2022-09-01 11:48:14 +0000

    refactor(controllers): used wrapped controller functions in createController util function

--- src/Controllers/ApiController.js
+++ src/Controllers/ApiController.js
@@ -1,64 +1,58 @@
 import passport from 'passport';
-import { isDevMode } from '../Lib/utils.js';
+import { isDevMode, createController } from '../Lib/utils.js';
 import InternalServerException from '../Exceptions/InternalServerException.js';
 import HttpAuthenticationException from '../Exceptions/HttpAuthenticationException.js';
 import InvalidatedJwt from '../Models/InvalidatedJwtModel.js';
 
 /**
  * @param {import("passport").PassportStatic} passport
- *
- * @returns {import("express").RequestHandler}
  */
-const JwtMiddleware = (passport) => (req, res, next) => {
-    passport.authenticate(
-        'jwt',
-        {
-            session: false,
-        },
-        async (error, user, info) => {
-            if (error) {
-                const response = {
-                    message: 'An error occured whiles performing request',
-                };
+const JwtMiddleware = (passport) =>
+    createController((req, res, next) => {
+        passport.authenticate(
+            'jwt',
+            {
+                session: false,
+            },
+            async (error, user, info) => {
+                if (error) {
+                    const response = {
+                        message: 'An error occured whiles performing request',
+                    };
 
-                if (isDevMode) {
-                    response.details = error;
+                    if (isDevMode) {
+                        response.details = error;
+                    }
+
+                    next(new InternalServerException(response));
                 }
 
-                next(new InternalServerException(response));
+                const token = req.headers.authorization?.split(' ')[1];
+                const tokenInvalidated = await InvalidatedJwt.findOne({
+                    token,
+                });
+
+                if (!token || tokenInvalidated) {
+                    next(new HttpAuthenticationException('Invalid JWT'));
+                    return;
+                }
+
+                if (!user) {
+                    next(
+                        new HttpAuthenticationException(
+                            info?.message ?? 'User not found'
+                        )
+                    );
+                }
+
+                req.user = user;
+
+                next();
             }
+        )(req, res, next);
+    });
 
-            const token = req.headers.authorization?.split(' ')[1];
-            const tokenInvalidated = await InvalidatedJwt.findOne({
-                token,
-            });
-
-            if (!token || tokenInvalidated) {
-                next(new HttpAuthenticationException('Invalid JWT'));
-                return;
-            }
-
-            if (!user) {
-                next(
-                    new HttpAuthenticationException(
-                        info?.message ?? 'User not found'
-                    )
-                );
-            }
-
-            req.user = user;
-
-            next();
-        }
-    )(req, res, next);
-};
-
-/**
- *
- * @param {import('express').Request} req
- * @param {import('express').Response} res
- */
-const logout = (req, res) => {
+const logout = createController((req, res) => {
     const token = req.headers.authorization?.split(' ')[1];
 
     InvalidatedJwt.findOneAndUpdate(
@@ -78,6 +72,6 @@ const logout = (req, res) => {
             message: 'Logout successful!',
         });
     });
-};
+});
 
 export { JwtMiddleware, logout };
