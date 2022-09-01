b9f19ce9596b04a43492454c068dd9e650d519d7
Mathias Ayivor <mathiasayivor5@gmail.com>
2022-09-01 11:53:51 +0000

    refactor(utils): used RequestHandler type from express instead of separate imports for each

--- src/Lib/utils.js
+++ src/Lib/utils.js
@@ -1,3 +1,5 @@
+import express from 'express';
+
 export function isDevMode() {
     if (process.env.NODE_ENV) {
         return /^dev(elopment)?$/i.test(process.env.NODE_ENV);
@@ -7,9 +9,9 @@ export function isDevMode() {
 }
 
 /**
- * @param {import("express").RequestHandler} controllerMethod
+ * @param {express.RequestHandler} controllerMethod
  *
- * @returns {import("express").RequestHandler}
+ * @returns {express.RequestHandler}
  */
 export function createController(controllerMethod) {
     return controllerMethod;
