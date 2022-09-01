cbd8b596654f66e3de4d21c2fbefc24c09c95ce7
Mathias Ayivor <mathiasayivor5@gmail.com>
2022-09-01 10:46:50 +0000

    refactor(authcontroller): extended jwt expiry to 30 days

--- src/Controllers/AuthController.js
+++ src/Controllers/AuthController.js
@@ -1,35 +1,44 @@
-import passport from "passport";
+import passport from 'passport';
 import jwt from 'jsonwebtoken';
-import { isDevMode } from "../Lib/utils.js";
-import InternalServerException from "../Exceptions/InternalServerException.js";
+import { isDevMode } from '../Lib/utils.js';
+import InternalServerException from '../Exceptions/InternalServerException.js';
 
 const twitterAuth = passport.authenticate('twitter');
 
 //this callback is called automatically after auth : reference => https://www.passportjs.org/packages/passport-twitter/
 /** @type {import("express").RequestHandler} */
 const twitterAuthCallback = (req, res, next) => {
-    passport.authenticate('twitter', {
-        assignProperty: 'federatedUser',
-        failureRedirect: 'http://localhost:5000/failure', // auth failure redirect,
-    }, async (err, user, info, status) => {
-        if (!user) {
-            throw new InternalServerException('An unknown error occured')
-        }
-
-        const token = jwt.sign({
-            user: {
-                id: user._id,
-                name: user.twitterUsername
+    passport.authenticate(
+        'twitter',
+        {
+            assignProperty: 'federatedUser',
+            failureRedirect: 'http://localhost:5000/failure', // auth failure redirect,
+        },
+        async (err, user, info, status) => {
+            if (!user) {
+                throw new InternalServerException('An unknown error occured');
             }
-        }, process.env.JWT_SECRET, {
-            expiresIn: '1h'
-        });
 
-        res.redirect(isDevMode() ? `http://localhost:5000/success?token=${token}` : `https://tweet-guard-frontend.vercel.app?token=${token}`) //auth success redirect
-    })(req, res, next)
-}
+            const token = jwt.sign(
+                {
+                    user: {
+                        id: user._id,
+                        name: user.twitterUsername,
+                    },
+                },
+                process.env.JWT_SECRET,
+                {
+                    expiresIn: '30 days',
+                }
+            );
 
-export {
-    twitterAuth,
-    twitterAuthCallback
-}
+            res.redirect(
+                isDevMode()
+                    ? `http://localhost:5000/success?token=${token}`
+                    : `https://tweet-guard-frontend.vercel.app?token=${token}`
+            ); //auth success redirect
+        }
+    )(req, res, next);
+};
+
+export { twitterAuth, twitterAuthCallback };
