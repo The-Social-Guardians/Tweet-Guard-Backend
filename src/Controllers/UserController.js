01b2214e0d2099c6df55d07b51637cabe1961d78
Luke Babich <luke@coldbrewstudios.co.uk>
2022-09-04 18:08:06 +0100

    feat(user routes): created base user router and controller with middleware

---
+++ src/Controllers/UserController.js
@@ -0,0 +1,13 @@
+import { createController } from '../Lib/utils.js';
+
+const getUserShamelist = createController((req, res) => {
+});
+
+const getWhitelist = createController((req, res) => {
+});
+
+const reportUser = createController((req, res) => {
+});
+
+export { getUserShamelist, getWhitelist, reportUser };
+
--- src/Lib/utils.js
+++ src/Lib/utils.js
@@ -1,5 +1,3 @@
-import express from 'express';
-
 export function isDevMode() {
     if (process.env.NODE_ENV) {
         return /^dev(elopment)?$/i.test(process.env.NODE_ENV);
